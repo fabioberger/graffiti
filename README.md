c1d46adec320c0e3c5f3c73a54e609c1a5d85453
Fabio Berger <fabioberger1991@gmail.com>
2015-02-19 13:02:47 -0800

    MVP after a night of fun and games

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-1
+# Write Any Message To Your Github Contributions
+
+This app will let you convert any text into git commits what will display the message on your Github profile's contributions visualizer
--- main.go
+++ main.go
@@ -1,60 +1,85 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"os"
 	"os/exec"
+	"time"
+)
+
+var (
+	git = "/usr/bin/git"
 )
 
 func main() {
 	projectName := os.Args[1]
+	message := os.Args[2]
 	createGitRepo(projectName)
-	numCommits := 2
-	createCommits(numCommits)
+	timestamps := getTimestamps(message)
+	formatCommitDates(timestamps)
 }
 
-func createCommits(numCommits int) {
+func formatCommitDates(timestamps []int64) {
+	// Loop over each timestamp to create 10 commits at it
+	for i, t := range timestamps {
+		// Loop over the corresponding 10 commits we need to edit
+		for j := i * 20; j < (i*20)+20; j++ {
+			createCommit()
+			date := time.Unix(t, 0)
+			formattedDate := date.Format("Mon Jan 2 15:04:05 2006 -0700")
+			c := exec.Command(git, "commit", "-m", "\"poof\"", "--amend", "--date", "\""+formattedDate+"\"")
+			c.Env = []string{"GIT_COMMITTER_DATE=\"" + formattedDate + "\""} //os.Environ()
+			err := c.Run()
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
 
-	// defer f.Close()
-	for i := 0; i < numCommits; i++ {
-		f, err := os.OpenFile("README.md", os.O_APPEND, 0666)
-		if err != nil {
-			panic(err)
-		}
-		f.Write([]byte{'1'})
-		buff := make([]byte, 50)
-		_, err = f.Read(buff)
-		f.Close()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("README contents: ", buff)
-		err = exec.Command("/usr/bin/git", "add", ".").Run()
-		if err != nil {
-			panic(err)
-		}
-		err = exec.Command("/usr/bin/git", "commit", "-m", "\"Poof\"").Run()
-		if err != nil {
-			panic(err)
-		}
+func getTimestamps(message string) []int64 {
+	holder := []int64{}
+	c := exec.Command("node", "./../../graffiti/index.js", message)
+	data, err := c.Output()
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(data, &holder)
+	return holder
+}
+
+func createCommit() {
+	f, err := os.OpenFile("README.md", os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("0")
+	err = exec.Command(git, "add", ".").Run()
+	if err != nil {
+		panic(err)
+	}
+	err = exec.Command(git, "commit", "-m", "\"Poof\"").Run()
+	if err != nil {
+		panic(err)
 	}
 }
 
 func createGitRepo(name string) {
 
 	// Create new project
-	os.Mkdir("./projects/"+name, 0666)
+	os.Mkdir("./projects/"+name, 0777)
 	os.Chdir("./projects/" + name)
 	f, err := os.Create("README.md")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	_, err = f.Write([]byte{'1'})
+	_, err = f.WriteString("1")
 	if err != nil {
 		panic(err)
 	}
-	err = exec.Command("/usr/bin/git", "init").Run()
+	err = exec.Command(git, "init").Run()
 	if err != nil {
 		panic(err)
 	}
